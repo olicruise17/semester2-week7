4787811985126b88766e35da936da3502dae44e4
Oli Cruise <cruiseoli@outlook.com>
2026-03-09 12:54:48 +0000

    End of session 6.1

--- session1/task1/crash.py
+++ session1/task1/crash.py
@@ -1,8 +1,10 @@
 # Run this program and observe what happens
 
 def assign_values(x):
-    for i in range(100):
+    for i in range(10):
         x[i] = (i + 1) * (i + 1);
+
+    print(x)
 
 if __name__ == "__main__":
     data = [0] * 10
